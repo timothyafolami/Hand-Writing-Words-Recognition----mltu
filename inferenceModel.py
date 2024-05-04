57a4fb7357ecd2fce280cf2dda7589961e45fd4e
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2024-05-04 23:12:32 +0100

    inference

--- inferenceModel.py
+++ inferenceModel.py
@@ -26,11 +26,11 @@ if __name__ == "__main__":
     from tqdm import tqdm
     from mltu.configs import BaseModelConfigs
 
-    configs = BaseModelConfigs.load("Models/03_handwriting_recognition/202301111911/configs.yaml")
+    configs = BaseModelConfigs.load("Models/03_handwriting_recognition/202405031124/configs.yaml")
 
     model = ImageToWordModel(model_path=configs.model_path, char_list=configs.vocab)
 
-    df = pd.read_csv("Models/03_handwriting_recognition/202301111911/val.csv").values.tolist()
+    df = pd.read_csv("Models/03_handwriting_recognition/202405031124/val.csv").values.tolist()
 
     accum_cer = []
     for image_path, label in tqdm(df):
